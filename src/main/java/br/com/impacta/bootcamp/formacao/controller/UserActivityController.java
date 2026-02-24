4b03c462eb0d4bd7b49b9d80ed256c54cb261771
Deivid F Venancio <deividvenancio@gmail.com>
2026-02-23 22:24:35 -0300

    ajuste logado modulo

--- src/main/java/br/com/impacta/bootcamp/formacao/controller/UserActivityController.java
+++ src/main/java/br/com/impacta/bootcamp/formacao/controller/UserActivityController.java
@@ -65,6 +65,7 @@ public class UserActivityController {
     @Monitorar
     @PostMapping(value = "/filtro/{pagina}")
     public JsonResponse filtro(@RequestBody List<SearchCriteriaDTO> lista,
+                               @RequestAttribute(value = "content") Content content,
                                @PathVariable("pagina") int pagina) {
         return JsonResponse.ok(userActivityService.getAll(lista, pagina));
     }
